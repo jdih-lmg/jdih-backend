c4206ac1d8c59638b774dcbdd57f50ca396f272f
Miftachul Huda <miftachulhd5@gmail.com>
2025-10-02 12:03:56 +0700

    mod: entity app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { HealthModule } from './health/health.module';
+import { User } from './entities/users.entity';
+import { Role } from './entities/roles.entity';
+import { Menu } from './entities/menus.entity';
+import { Action } from './entities/actions.entity';
+import { RoleMenuPermission } from './entities/role-menu-permissions.entity';
+import { Document } from './entities/documents.entity';
+import { DocumentCategory } from './entities/document-categories.entity';
+import { DocumentVersion } from './entities/document-versions.entity';
+import { AuditLog } from './entities/audit-logs.entity';
 
 @Module({
   imports: [
@@ -18,7 +27,17 @@ import { HealthModule } from './health/health.module';
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       synchronize: false, // ubah false pada production
-      entities: ['dist/entities/**/*{.js,.ts}', 'src/entities/**/*{.ts,.js}'],
+      entities: [
+        User,
+        Role,
+        Menu,
+        Action,
+        RoleMenuPermission,
+        Document,
+        DocumentCategory,
+        DocumentVersion,
+        AuditLog,
+      ],
       autoLoadEntities: true,
     }),
     CommonModule.forRoot(),
