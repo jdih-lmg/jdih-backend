f2d088da9d21107f7f3292aaa621fe70ff7bc372
Miftachul Huda <miftachulhd5@gmail.com>
2025-10-03 10:51:03 +0700

    add: exception filters global

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 import type { CreateUserDto } from './dto/create-user.dto';
+import type { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -8,28 +9,40 @@ export class UsersController {
 
   // Get all users
   @Get()
-  getAllUserController() {
-    return this.userService.getAllUserService();
+  async getAllUserController() {
+    const data = await this.userService.getAllUserService();
+    return {
+      message: 'Berhasil mendapatkan semua user',
+      success: true,
+      data,
+    };
   }
 
   // Get user by id
   @Get(':id')
-  getUserByIdController(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.getUserByIdService(id);
+  async getUserByIdController(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.userService.getUserByIdService(id);
+    return { message: `Berhasil mendapatkan user id ${id}`, success: true, data: user };
   }
 
   // Create user
-  createUserController(@Body() dto: CreateUserDto) {
-    return this.userService.createUserService(dto);
+  @Post()
+  async createUserController(@Body() dto: CreateUserDto) {
+    const user = await this.userService.createUserService(dto);
+    return { message: 'Berhasil membuat user', success: true, data: user };
   }
 
   // Update user by id
-  updateUserController(@Param('id', ParseIntPipe) id: number, @Body() dto: CreateUserDto) {
-    return this.userService.updateUserService(id, dto);
+  @Put(':id')
+  async updateUserController(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateUserDto) {
+    const user = await this.userService.updateUserService(id, dto);
+    return { message: `Berhasil mengupdate user id ${id}`, success: true, data: user };
   }
 
   // Delete user by id
-  delteUserController(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.deleteUserService(id);
+  @Delete(':id')
+  async delteUserController(@Param('id', ParseIntPipe) id: number) {
+    await this.userService.deleteUserService(id);
+    return { message: `Berhasil menghapus user id ${id}`, success: true, data: null };
   }
 }
