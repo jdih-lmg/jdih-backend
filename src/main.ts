469f79f77081e093613a24d70165055882983931
Miftachul Huda <71609795+knock5@users.noreply.github.com>
2025-11-22 09:32:45 +0700

    Enhance bootstrap function with NestExpressApplication

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { AllExceptionFilter } from './common/filters/all-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { Response } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.use(helmet());
   app.enableCors();
-  // Set global prefix for all routes
   app.setGlobalPrefix('api');
 
-  // Register global error filter sederhana
   app.useGlobalFilters(new AllExceptionFilter());
+
+  const uploadsDir = join(__dirname, '..', process.env.UPLOADS_PATH || 'uploads');
+
+  app.useStaticAssets(uploadsDir, {
+    prefix: '/uploads/',
+    setHeaders: (res: Response, path: string) => {
+      if (path.endsWith('.pdf')) {
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+      }
+    },
+  });
 
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port);
-  console.log(`Server berjalan di http://localhost:${port}`);
+  console.log(`✅ Server berjalan di http://localhost:${port}`);
 }
 
 bootstrap().catch((err) => {
